bf07c6e75b91d67cc77d84e4539addd79931ca66
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2022-04-17 12:34:38 +0200

    dev: login endpoint progress

--- backend/routes/token.ts
+++ backend/routes/token.ts
@@ -1,9 +1,15 @@
 import Router from "@koa/router";
 import { API_VERSION } from "@tmp/back/routes";
+import useSingletonService from "@tmp/back/service/singleton-service";
 
 const router = new Router({ prefix: `/api/${API_VERSION}/token` });
 
 router.get("/login", async (req, res) => {
+    const { getPassword } = useSingletonService();
+    const currentPassword = await getPassword();
+
+    if (currentPassword === null) req.status = 400;
+
     req.body = "XDD";
 });
 
--- backend/service/singleton-service.ts
+++ backend/service/singleton-service.ts
@@ -25,7 +25,7 @@ const useSingletonService = () => {
      * @param password password to be encrypted and saved in db
      * @returns true if operation was successfull and false otherwise
      */
-    const setPassword = async (password: string) => {
+    const setPassword = async (password: string): Promise<boolean> => {
         const encryptedPassword = await bcrypt.hash(password, 10);
         const currentPassword = await getPasswordFromDb();
 
@@ -34,6 +34,22 @@ const useSingletonService = () => {
         } else {
             return changePasswordAtDb(encryptedPassword);
         }
+    };
+
+    /**
+     * Function to compare provided passwrods with password set in db.
+     * If there is no password in db false will be returned
+     * @param password password to be validated
+     * @returns if provided password is correct one
+     */
+    const comparePasswords = async (password: string): Promise<boolean> => {
+        const currentPassword = await getPasswordFromDb();
+        if (currentPassword === null) {
+            return false;
+        }
+        const result = await bcrypt.compare(password, currentPassword);
+
+        return result;
     };
 
     return { getPassword, setPassword };
