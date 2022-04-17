59d779a1af911b051451b875fb55f37040d9d301
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2022-04-18 00:25:59 +0200

    dev: tring image on loging form

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import { Button, Card, Container, Form } from "react-bootstrap";
+import xd from "./cat-closed-eyes-wide-open.png";
 
 function LoginPage() {
     return (
         <Container className="bg-light p-5" style={{ height: "100vh" }} fluid>
-            <Card className="px-3 py-4 mx-auto" style={{ maxWidth: "32rem" }}>
+            <Card
+                className="px-3 py-4 mx-auto"
+                style={{ maxWidth: "32rem", position: "relative" }}
+            >
                 <h1>
                     <strong>THE TMP APP!</strong>
                 </h1>
-
+                <img
+                    src={xd}
+                    style={{ position: "absolute", right: 0, top: "-30px" }}
+                    width="200px"
+                />
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Admin password</Form.Label>
                     <Form.Control
