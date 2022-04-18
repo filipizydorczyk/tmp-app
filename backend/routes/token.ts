f3ffbac120164bb1f816a8c5412b255b71284b7c
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2022-04-19 01:06:23 +0200

    dev: setup for tokens

--- backend/routes/token.ts
+++ backend/routes/token.ts
@@ -1,3 +1,4 @@
+import jwt from "jsonwebtoken";
 import Router from "@koa/router";
 import { API_VERSION } from "@tmp/back/routes";
 import useSingletonService from "@tmp/back/services/singleton-service";
@@ -12,15 +13,37 @@ router.get("/login", async (req, res) => {
     const currentPassword = await getPassword();
     const providedPassword = req.body.password;
 
+    let responseStatus = 401;
+
     if (currentPassword === null) {
         const isPasswordSet = await setPassword(providedPassword);
-        req.status = isPasswordSet ? 200 : 401;
+        responseStatus = isPasswordSet ? 200 : 401;
     } else {
         const result = await comparePasswords(providedPassword);
-        req.status = result ? 200 : 401;
+        responseStatus = result ? 200 : 401;
     }
 
-    req.body = "XDD";
+    // TODO process.env.REFRESH_TOKEN_SECRET
+
+    req.status = responseStatus;
+    req.body = {
+        message:
+            responseStatus === 401
+                ? "Provided password was not correct"
+                : "Successfully loged in!",
+        accessToken:
+            responseStatus === 200
+                ? jwt.sign(providedPassword, "I-will-add-token-here", {
+                      expiresIn: "15m",
+                  })
+                : null,
+        refreshToken:
+            responseStatus === 200
+                ? jwt.sign(providedPassword, "I-will-add-token-here", {
+                      expiresIn: "20m",
+                  })
+                : null,
+    };
 });
 
 export default router;
