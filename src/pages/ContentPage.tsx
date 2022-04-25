0887d882462b1cd591aa6a4999e562bc3db23e98
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2022-04-25 13:16:22 +0200

    dev: first overview component

--- src/pages/ContentPage.tsx
+++ src/pages/ContentPage.tsx
@@ -1,13 +1,24 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Col, Container, Form, Row } from "react-bootstrap";
 
 function ContentPage() {
     return (
-        <Container
-            className="bg-light p-5"
-            style={{ height: "100vh" }}
-            fluid
-        ></Container>
+        <Container className="bg-light p-5" style={{ height: "100vh" }} fluid>
+            <Row>
+                <Col></Col>
+                <Col>
+                    <Form>
+                        <Form.Group
+                            className="mb-3"
+                            controlId="exampleForm.ControlTextarea1"
+                        >
+                            <Form.Label>Temporary notes</Form.Label>
+                            <Form.Control as="textarea" rows={3} />
+                        </Form.Group>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
