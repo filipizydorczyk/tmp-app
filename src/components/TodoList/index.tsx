891de65226e21853c8522deb614aa9711be05ac1
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2022-04-26 14:00:21 +0200

    ref: type udpate

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -4,7 +4,7 @@ export type TodoListElement = {
     id: string;
     title: string;
     date: Date;
-    isDone: string;
+    done: boolean;
 };
 
 export type TodoListElementActionCallback = (callbackArgs: {
