60631d44f1014886b9da7f6226d0e3142452f5c2
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2022-05-06 17:26:48 +0200

    ref: working on securioty module - not ready to test

--- backend/security.ts
+++ backend/security.ts
@@ -1,3 +1,11 @@
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
+
+const ACCESS_TOKEN_SECRET =
+    process.env.ACCESS_TOKEN_SECRET || crypto.randomBytes(64).toString("hex");
+const REFRESH_TOKEN_SECRET =
+    process.env.REFRESH_TOKEN_SECRET || crypto.randomBytes(64).toString("hex");
+
 export type TokenResponse = {
     accessToken: string | null;
     refreshToken: string | null;
@@ -9,7 +17,7 @@ export type TokenResponse = {
  * manage tokens
  */
 export const useSecurity = () => {
-    const refreshTokens = [];
+    let refreshTokens: string[] = [];
 
     /**
      * Function to obtain access and refresh tokens and
@@ -18,25 +26,41 @@ export const useSecurity = () => {
      * @returns accessToken and refreshToken
      */
     const login = (password: string): TokenResponse => {
-        return { accessToken: null, refreshToken: null };
+        const accessToken = jwt.sign({ password }, ACCESS_TOKEN_SECRET, {
+            expiresIn: "15m",
+        });
+        const refreshToken = jwt.sign({ password }, REFRESH_TOKEN_SECRET, {
+            expiresIn: "20m",
+        });
+        refreshTokens.push(refreshToken);
+
+        return {
+            accessToken,
+            refreshToken,
+        };
     };
 
     /**
      * Function to clear refresh token and in consequence
      * stop authorizing requests with this token
-     * @param refreshTokens token to be removed
+     * @param token refresh token to be removed
      * @returns if operation was successful
      */
-    const logout = (refreshTokens: string): boolean => {
+    const logout = (token: string): boolean => {
         return false;
     };
 
     /**
      * Function to refresh expired token
-     * @param refreshTokens token to be refreshed
+     * @param token refresh token to be used
      * @returns accessToken and refreshToken
      */
-    const refresh = (refreshTokens: string): TokenResponse => {
+    const refresh = (token: string): TokenResponse => {
+        if (!refreshTokens.includes(token)) {
+            return { accessToken: null, refreshToken: null };
+        }
+        refreshTokens = refreshTokens.filter((tk) => tk !== token);
+
         return { accessToken: null, refreshToken: null };
     };
 
