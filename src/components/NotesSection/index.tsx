55128e0b6b14983621f300ca472a622b562e58df
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2022-05-08 12:37:56 +0200

    dev: toast integrated with api response

--- src/components/NotesSection/index.tsx
+++ src/components/NotesSection/index.tsx
@@ -1,10 +1,11 @@
 import { useNotes } from "@tmp/front/contexts/notes-context";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Button, Form, Toast } from "react-bootstrap";
 
 function NotesSection() {
     const notes = useNotes();
     const notesRef = useRef<HTMLTextAreaElement>(null);
+    const [resultToast, setResultToast] = useState<boolean>(false);
 
     useEffect(() => {
         notes.fetchNotes();
@@ -27,27 +28,29 @@ function NotesSection() {
                         className="my-2"
                         variant="primary"
                         onClick={() => {
-                            notes.saveNotes(notesRef.current?.value || "");
+                            notes
+                                .saveNotes(notesRef.current?.value || "")
+                                .then((_) => {
+                                    setResultToast(true);
+                                });
                         }}
                     >
                         Save
                     </Button>
                 </Form.Group>
             </Form>
+
             <Toast
                 className="my-4 mx-5"
                 style={{ position: "absolute", bottom: "0", right: 0 }}
+                onClose={() => setResultToast(false)}
+                show={resultToast}
+                animation={true}
             >
                 <Toast.Header>
-                    <img
-                        src="holder.js/20x20?text=%20"
-                        className="rounded me-2"
-                        alt=""
-                    />
-                    <strong className="me-auto">Bootstrap</strong>
-                    <small>11 mins ago</small>
+                    <strong className="me-auto">Notes saved</strong>
                 </Toast.Header>
-                <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+                <Toast.Body>Click to close this message.</Toast.Body>
             </Toast>
         </>
     );
