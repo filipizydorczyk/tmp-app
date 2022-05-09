10cceda9819958ff569905773fe4f6487127e10d
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2022-05-09 14:28:36 +0200

    dev: get all maybe implementation??

--- backend/repositories/task-repo.ts
+++ backend/repositories/task-repo.ts
@@ -1,14 +1,30 @@
+import { getDatabase, TASK_TABLE_NAME } from "@tmp/back/db";
+
+export type TaskEntity = {
+    id: string;
+    title: string;
+    date: string;
+    done: 0 | 1;
+};
+
 const useTaskRepository = () => {
-    const getAllTasks = () => {};
-    const createTask = (values: {
-        title: string;
-        date: string;
-        done: 0 | 1;
-    }) => {};
-    const updateTask = (
-        id: string,
-        values: { title: string; date: string; done: 0 | 1 }
-    ) => {};
+    const getAllTasks = (): Promise<TaskEntity[]> => {
+        return new Promise((resolve, reject) => {
+            const db = getDatabase();
+
+            db.get(`SELECT * FROM ${TASK_TABLE_NAME}"`, (err, row) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(row as TaskEntity[]);
+                }
+            });
+
+            db.close();
+        });
+    };
+    const createTask = ({ title, date, done }: TaskEntity) => {};
+    const updateTask = ({ id, title, date, done }: TaskEntity) => {};
     const deleteTask = (id: string) => {};
 
     return {
