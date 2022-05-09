c85c0631e9cf6aa434b4d294c8be116d52990a37
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2022-05-09 13:09:39 +0200

    dev: table for tasks

--- backend/db.ts
+++ backend/db.ts
@@ -3,6 +3,7 @@ import path from "path";
 
 export const DB_PATH = path.join(__dirname, "../database.db");
 export const SINGLETON_TABLE_NAME = "Singletons";
+export const TASK_TABLE_NAME = "Tasks";
 
 const sqlite = sqlite3.verbose();
 
@@ -11,6 +12,9 @@ export const getDatabase = () => {
 
     db.run(
         `CREATE TABLE IF NOT EXISTS ${SINGLETON_TABLE_NAME} (Key TEXT NOT NULL, Value TEXT NOT NULL)`
+    );
+    db.run(
+        `CREATE TABLE IF NOT EXISTS ${TASK_TABLE_NAME} (Id TEXT NOT NULL, Title TEXT NOT NULL, Date TEXT NOT NULL, Done INTEGER NOT NULL)`
     );
 
     return new sqlite.Database(DB_PATH);
