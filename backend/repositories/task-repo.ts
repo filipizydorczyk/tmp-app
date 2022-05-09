a204c09d097359951f4686d123e1b137c7f542bf
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2022-05-09 13:16:55 +0200

    dev: listed functions for task repo to be implemented

---
+++ backend/repositories/task-repo.ts
@@ -0,0 +1,13 @@
+const useTaskRepository = () => {
+    const getAllTasks = () => {};
+    const createTask = () => {};
+    const updateTask = (id: string) => {};
+    const deleteTask = (id: string) => {};
+
+    return {
+        getAllTasks,
+        createTask,
+        updateTask,
+        deleteTask,
+    };
+};
