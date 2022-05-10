dbd1a6242058096f56f1f99bc08e0f70db4d5ddd
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2022-05-10 12:45:55 +0200

    dev: added methots to implement

--- backend/services/task-service.ts
+++ backend/services/task-service.ts
@@ -1,11 +1,15 @@
 import { TaskEntity, TaskRepository } from "@tmp/back/repositories/task-repo";
+import { TaskDTO } from "@tmp/back/routes/tasks";
 
-const useTaskService = (repository: TaskRepository) => {
+export const useTaskService = (repository: TaskRepository) => {
     const getTasks = (): Promise<TaskEntity[]> => {
         return repository.getAllTasks();
     };
+    const changeTaksStatus = (id: string) => {};
+    const deleteTask = (id: string) => {};
+    const updateTask = (task: TaskDTO) => {};
 
-    return { getTasks };
+    return { getTasks, changeTaksStatus, deleteTask, updateTask };
 };
 
 export default useTaskService;
