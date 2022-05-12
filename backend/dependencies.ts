b1c2c9651b84f7f7b5ef6d5cc74f0398723a8b0c
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2022-05-12 22:29:06 +0200

    ref: import refactor

--- backend/dependencies.ts
+++ backend/dependencies.ts
@@ -2,7 +2,7 @@ import { AppDependencies } from "@tmp/back/app";
 import useSingletonRepository from "@tmp/back/repositories/singleton-repo";
 import useSingletonService from "@tmp/back/services/singleton-service";
 import useTaskRepository from "@tmp/back/repositories/task-repo";
-import useTaskService from "./services/task-service";
+import useTaskService from "@tmp/back/services/task-service";
 
 /**
  * Function to create app dependencies needed in routes
