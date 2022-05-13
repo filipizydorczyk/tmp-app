ac736a5daab2a3939745daee3a05b06fc6e42872
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2022-05-13 20:57:00 +0200

    dev: tasks to be implemented

---
+++ tests/routes/task.test.ts
@@ -0,0 +1,10 @@
+const ROUTER_PREFIX = "/api/v1/tasks";
+
+describe(`API ${ROUTER_PREFIX}`, () => {
+    it("should return all tasks", (done) => {});
+    it("should fail creating task with invalid body", (done) => {});
+    it("should create task", (done) => {});
+    it("should update task", (done) => {});
+    it("should fail updating with invalid body", (done) => {});
+    it("should delete task", (done) => {});
+});
