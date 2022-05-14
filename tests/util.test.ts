4034f36454caebc5b47583a4594318db98dc43b6
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2022-05-14 20:22:42 +0200

    dev: utils tests

---
+++ tests/util.test.ts
@@ -0,0 +1,18 @@
+import assert from "assert";
+import { isIsoDate } from "@tmp/back/util";
+
+describe(`Utils tests`, () => {
+    it("should validate correct iso string", () => {
+        assert.ok(isIsoDate("2022-05-12T19:46:25.667Z"));
+    });
+
+    it("should invalidate incorrect iso string", () => {
+        assert.ok(!isIsoDate("2022-05-12T19:46:25.667X"));
+    });
+    it("should invalidate nonsense string", () => {
+        assert.ok(!isIsoDate("fasdfsdf"));
+    });
+    it("should invalidate empty string", () => {
+        assert.ok(!isIsoDate(""));
+    });
+});
