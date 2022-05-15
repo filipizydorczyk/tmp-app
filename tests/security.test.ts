d16b0d76789d0c8a63cfc4f0e5b0ab3c5b932282
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2022-05-15 18:35:27 +0200

    dev: secutiry tests defined

--- tests/security.test.ts
+++ tests/security.test.ts
@@ -2,16 +2,13 @@ import assert from "assert";
 import jwt from "jsonwebtoken";
 
 describe.only(`Security tests`, () => {
-    it("should", () => {
-        const token = jwt.sign({ password: "lmao" }, "test", {
-            expiresIn: "15m",
-        });
-        jwt.verify(`${token}-xD`, "test", (err, user) => {
-            if (err) {
-                console.log("Error", err);
-            } else {
-                console.log("Success", user);
-            }
-        });
-    });
+    it("should login with correct creadentials", () => {});
+    it("should create passwrod if there is no password yet", () => {});
+    it("should reject if password is incorect", () => {});
+    it("should validate correct token", () => {});
+    it("should invalidate incorrect token", () => {});
+    it("should logout", () => {});
+    it("should not logout if refresh token doesnt exist", () => {});
+    it("should refresh token", () => {});
+    it("should fail refreshing with incorect token", () => {});
 });
