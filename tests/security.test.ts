8fc666b663224946c84e40a922c2aafa9ff8962b
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2022-05-16 20:46:09 +0200

    dev: security login positive test

--- tests/security.test.ts
+++ tests/security.test.ts
@@ -1,14 +1,50 @@
+import { SingletonRepository } from "@tmp/back/repositories/singleton-repo";
+import useSecurity from "@tmp/back/security";
+import useSingletonService from "@tmp/back/services/singleton-service";
 import assert from "assert";
 import jwt from "jsonwebtoken";
+import sinon from "sinon";
+
+const TEST_PASSWORD = "test";
 
 describe.only(`Security tests`, () => {
-    it("should login with correct creadentials", () => {});
-    it("should create passwrod if there is no password yet", () => {});
-    it("should reject if password is incorect", () => {});
-    it("should validate correct token", () => {});
-    it("should invalidate incorrect token", () => {});
-    it("should logout", () => {});
-    it("should not logout if refresh token doesnt exist", () => {});
-    it("should refresh token", () => {});
-    it("should fail refreshing with incorect token", () => {});
+    it("should login with correct creadentials", async () => {
+        const service = useSingletonService({} as SingletonRepository);
+        sinon
+            .stub(service, "getPassword")
+            .returns(Promise.resolve(TEST_PASSWORD));
+        sinon.stub(service, "setPassword").returns(Promise.resolve(false));
+        sinon.stub(service, "comparePasswords").returns(Promise.resolve(true));
+
+        const { login } = useSecurity(service);
+        const response = await login(TEST_PASSWORD);
+        assert.deepEqual(response.type, "login");
+        assert.ok(response.tokens.accessToken !== null);
+        assert.ok(response.tokens.refreshToken !== null);
+    });
+
+    it("should create passwrod if there is no password yet", () => {
+        assert.ok(false);
+    });
+    it("should reject if password is incorect", () => {
+        assert.ok(false);
+    });
+    it("should validate correct token", () => {
+        assert.ok(false);
+    });
+    it("should invalidate incorrect token", () => {
+        assert.ok(false);
+    });
+    it("should logout", () => {
+        assert.ok(false);
+    });
+    it("should not logout if refresh token doesnt exist", () => {
+        assert.ok(false);
+    });
+    it("should refresh token", () => {
+        assert.ok(false);
+    });
+    it("should fail refreshing with incorect token", () => {
+        assert.ok(false);
+    });
 });
