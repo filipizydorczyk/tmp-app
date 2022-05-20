b69a045858e776b2ca3f809723e7f4d8347d3ea9
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2022-05-20 15:45:31 +0200

    dev: overview routing when user is logged out

--- src/pages/ContentPage.tsx
+++ src/pages/ContentPage.tsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { NotesProvider } from "@tmp/front/contexts/notes-context";
 import NotesSection from "@tmp/front/components/NotesSection";
 import { TaskProvider } from "@tmp/front/contexts/tasks-context";
+import { useAuth } from "@tmp/front/contexts/auth-context";
+import { useNavigate } from "react-router-dom";
 import TodoSection from "@tmp/front/components/TodoSection";
+import { LOGIN_URL } from "@tmp/front/constants";
 
 function ContentPage() {
+    const { data } = useAuth();
+    const nav = useNavigate();
+
+    useEffect(() => {
+        if (!data.isLoggedIn) {
+            nav(LOGIN_URL);
+        }
+    }, []);
+
     return (
         <Container className="bg-light p-5" style={{ height: "100vh" }} fluid>
             <Row style={{ height: "100%" }}>
