36e0885fbcf05e2548b2a4a053361173deeaf4d4
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2022-05-22 20:03:11 +0200

    ref: better directory structure

--- craco.config.js
+++ craco.config.js
@@ -2,7 +2,34 @@ const path = require("path");
 module.exports = {
     webpack: {
         alias: {
-            "@tmp/front": path.resolve(__dirname, "src/"),
+            "@tmp/front": path.resolve(__dirname, "frontend/"),
+        },
+        configure: function (webpackConfig, { paths, env }) {
+            paths.appIndexJs = path.resolve(__dirname, "frontend/index.tsx");
+            paths.appSrc = path.resolve(__dirname, "frontend");
+            webpackConfig.entry = paths.appIndexJs;
+
+            webpackConfig.module.rules.push({
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: require.resolve("babel-loader"),
+                        options: {
+                            presets: [
+                                require("@babel/preset-typescript").default,
+                                [
+                                    require("@babel/preset-react").default,
+                                    { runtime: "automatic" },
+                                ],
+                                require("@babel/preset-env").default,
+                            ],
+                        },
+                    },
+                ],
+            });
+
+            return webpackConfig;
         },
     },
 };
