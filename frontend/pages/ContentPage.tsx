051da00083fbf8be87ad4f842b532681c0318737
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2022-06-26 21:09:30 +0200

    dev: mobile view setup

--- frontend/pages/ContentPage.tsx
+++ frontend/pages/ContentPage.tsx
@@ -7,10 +7,26 @@ import { useAuth } from "@tmp/front/contexts/auth-context";
 import { useNavigate } from "react-router-dom";
 import TodoSection from "@tmp/front/components/TodoSection";
 import { LOGIN_URL } from "@tmp/front/constants";
+import { createMedia } from "@artsy/fresnel";
 
 function ContentPage() {
     const { data } = useAuth();
     const nav = useNavigate();
+    const QueryBreakpoints = createMedia({
+        // breakpoints values can be either strings or integers
+        breakpoints: {
+            sm: 0,
+            md: 700,
+        },
+    });
+
+    // workaround to fresnel type issue
+    const MediaContextProvider =
+        QueryBreakpoints.MediaContextProvider as unknown as React.FC;
+    const Media = QueryBreakpoints.Media as unknown as React.FC<{
+        at?: any;
+        greaterThanOrEqual?: any;
+    }>;
 
     useEffect(() => {
         if (!data.isLoggedIn) {
@@ -24,18 +40,25 @@ function ContentPage() {
             style={{ height: "100vh", maxHeight: "100vh" }}
             fluid
         >
-            <Row style={{ height: "100%" }}>
-                <Col style={{ height: "100%" }}>
-                    <TaskProvider>
-                        <TodoSection style={{ height: "100%" }} />
-                    </TaskProvider>
-                </Col>
-                <Col style={{ height: "100%" }}>
-                    <NotesProvider>
-                        <NotesSection />
-                    </NotesProvider>
-                </Col>
-            </Row>
+            <MediaContextProvider>
+                <Media at="sm">
+                    <h1>Hello worlds</h1>
+                </Media>
+                <Media greaterThanOrEqual="md">
+                    <Row style={{ height: "100%" }}>
+                        <Col style={{ height: "100%" }}>
+                            <TaskProvider>
+                                <TodoSection style={{ height: "100%" }} />
+                            </TaskProvider>
+                        </Col>
+                        <Col style={{ height: "100%" }}>
+                            <NotesProvider>
+                                <NotesSection />
+                            </NotesProvider>
+                        </Col>
+                    </Row>
+                </Media>
+            </MediaContextProvider>
         </Container>
     );
 }
