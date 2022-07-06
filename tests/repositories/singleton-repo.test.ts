d2de4c8332ae88ccdf6b9bc2fc4db866fade3450
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2022-07-06 20:52:30 +0200

    dev: notes flow integration tests

--- tests/repositories/singleton-repo.test.ts
+++ tests/repositories/singleton-repo.test.ts
@@ -5,7 +5,7 @@ import useSingletonRepository from "@tmp/back/repositories/singleton-repo";
 import path from "path";
 
 describe(`SingletonRepository integration tests`, async () => {
-    it.only("password flow", async () => {
+    it("password flow", async () => {
         const dbPath = path.join(__dirname, `../artifacts/${uuid4()}.db`);
 
         const repo = useSingletonRepository(dbPath);
@@ -25,6 +25,30 @@ describe(`SingletonRepository integration tests`, async () => {
         password = await repo.getPassword();
         assert.deepEqual(true, result);
         assert.deepEqual(password, "second-passwd");
+
+        fs.rmSync(dbPath);
+    });
+
+    it("notes flow", async () => {
+        const dbPath = path.join(__dirname, `../artifacts/${uuid4()}.db`);
+
+        const repo = useSingletonRepository(dbPath);
+
+        let notes = await repo.getNotes();
+        let result = false;
+        assert.deepEqual(notes, null);
+
+        result = await repo.setNotes("First note!");
+        notes = await repo.getNotes();
+        assert.deepEqual(true, result);
+        assert.deepEqual(notes, "First note!");
+
+        result = false;
+
+        result = await repo.updateNotes("Second note!");
+        notes = await repo.getNotes();
+        assert.deepEqual(true, result);
+        assert.deepEqual(notes, "Second note!");
 
         fs.rmSync(dbPath);
     });
