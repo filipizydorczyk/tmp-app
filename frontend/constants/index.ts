0bdde9d0d29b9d91e2eb3bcf49aa6ab36cfcbbf5
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2022-12-27 16:38:20 +0100

    dev: 1.0.7 - better moblie responsivness

--- frontend/constants/index.ts
+++ frontend/constants/index.ts
@@ -1,2 +1,12 @@
 export const LOGIN_URL = "/login";
 export const OVERVIEW_URL = "/overview";
+
+export const DAYS = [
+  { name: "Monday", short: ["Mon.", "Mo."] },
+  { name: "Tuesday", short: ["Tue.", "Tu."] },
+  { name: "Wednesday", short: ["Wed.", "We."] },
+  { name: "Thursday", short: ["Thu.", "Th."] },
+  { name: "Friday", short: ["Fri.", "Fr."] },
+  { name: "Saturday", short: ["Sat.", "Sa."] },
+  { name: "Sunday", short: ["Sun.", "Su."] },
+] as const;
--- frontend/pages/ContentPage.tsx
+++ frontend/pages/ContentPage.tsx
@@ -8,74 +8,76 @@ import { useNavigate } from "react-router-dom";
 import TodoSection from "@tmp/front/components/TodoSection";
 import { LOGIN_URL } from "@tmp/front/constants";
 import { createMedia } from "@artsy/fresnel";
+import Timeline from "../components/Timeline";
 
 function ContentPage() {
-    const { data } = useAuth();
-    const nav = useNavigate();
-    const QueryBreakpoints = createMedia({
-        breakpoints: {
-            sm: 0,
-            md: 700,
-        },
-    });
+  const { data } = useAuth();
+  const nav = useNavigate();
+  const QueryBreakpoints = createMedia({
+    breakpoints: {
+      sm: 0,
+      md: 700,
+    },
+  });
 
-    // workaround to fresnel type issue (https://github.com/artsy/fresnel/issues/281)
-    const MediaContextProvider =
-        QueryBreakpoints.MediaContextProvider as unknown as React.FC;
-    const Media = QueryBreakpoints.Media as unknown as React.FC<{
-        at?: any;
-        greaterThanOrEqual?: any;
-    }>;
+  // workaround to fresnel type issue (https://github.com/artsy/fresnel/issues/281)
+  const MediaContextProvider =
+    QueryBreakpoints.MediaContextProvider as unknown as React.FC;
+  const Media = QueryBreakpoints.Media as unknown as React.FC<{
+    at?: any;
+    greaterThanOrEqual?: any;
+  }>;
 
-    const TaskFragment = (
-        <TaskProvider>
-            <TodoSection style={{ height: "100%" }} />
-        </TaskProvider>
-    );
-    const TodoFragment = (
-        <NotesProvider>
-            <NotesSection />
-        </NotesProvider>
-    );
+  const TaskFragment = (
+    <TaskProvider>
+      <TodoSection style={{ height: "100%" }} />
+    </TaskProvider>
+  );
+  const TodoFragment = (
+    <NotesProvider>
+      {/* <Timeline/> */}
+      <NotesSection />
+    </NotesProvider>
+  );
 
-    useEffect(() => {
-        if (!data.isLoggedIn) {
-            nav(LOGIN_URL);
-        }
-    }, []);
+  useEffect(() => {
+    if (!data.isLoggedIn) {
+      nav(LOGIN_URL);
+    }
+  }, []);
 
-    return (
-        <MediaContextProvider>
-            <Media at="sm">
-                <Container
-                    className="bg-light py-2 px-4"
-                    style={{ height: "100vh", maxHeight: "100vh" }}
-                    fluid
-                >
-                    <Tabs defaultActiveKey="tasks" className="mb-3">
-                        <Tab eventKey="tasks" title="Tasks">
-                            {TaskFragment}
-                        </Tab>
-                        <Tab eventKey="notes" title="Notes">
-                            {TodoFragment}
-                        </Tab>
-                    </Tabs>
-                </Container>
-            </Media>
-            <Media greaterThanOrEqual="md">
-                <Container
-                    className="bg-light p-5"
-                    style={{ height: "100vh", maxHeight: "100vh" }}
-                    fluid
-                >
-                    <Row style={{ height: "100%" }}>
-                        <Col style={{ height: "100%" }}>{TaskFragment}</Col>
-                        <Col style={{ height: "100%" }}>{TodoFragment}</Col>
-                    </Row>
-                </Container>
-            </Media>
-        </MediaContextProvider>
-    );
+  return (
+    <MediaContextProvider>
+      <Media at="sm">
+        <Container
+          className="bg-light py-2 px-4"
+          style={{ height: "100vh", maxHeight: "100vh" }}
+          fluid
+        >
+          <Tabs defaultActiveKey="tasks" className="mb-3">
+            <Tab eventKey="tasks" title="Tasks">
+              <div style={{ height: "85vh" }}>{TaskFragment}</div>
+            </Tab>
+            <Tab eventKey="notes" title="Notes">
+              <div style={{ height: "85vh" }}>{TodoFragment}</div>
+            </Tab>
+          </Tabs>
+        </Container>
+      </Media>
+      <Media greaterThanOrEqual="md">
+        <Container
+          className="bg-light p-5"
+          style={{ height: "100vh", maxHeight: "100vh" }}
+          fluid
+        >
+          <Row style={{ height: "100%" }}>
+            <Col style={{ height: "100%" }}>{TaskFragment}</Col>
+            <Col style={{ height: "100%" }}>{TodoFragment}</Col>
+          </Row>
+        </Container>
+      </Media>
+    </MediaContextProvider>
+  );
 }
 
 export default ContentPage;
